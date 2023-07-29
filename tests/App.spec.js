0aa8e4665d20dfb69512602dafc633cf19f5474c
Alexandra Skripkina <aa.skripkina@gmail.com>
2023-07-29 11:25:07 +0300

    Initial commit

---
+++ tests/App.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+import { email, password } from "../user";
+
+test("Valid test", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.locator('[placeholder="Email"]').click();
+  await page.locator('[placeholder="Email"]').fill(email);
+  await page.locator('[placeholder="Пароль"]').click();
+  await page.locator('[placeholder="Пароль"]').fill(password);
+  await page.locator('[data-testid="login-submit-btn"]').click();
+  await expect(page).toHaveURL("https://netology.ru/profile");
+});
+
+test("Failure test", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.locator('[placeholder="Email"]').click();
+  await page.locator('[placeholder="Email"]').fill("aivanov@mail.ru");
+  await page.locator('[placeholder="Пароль"]').click();
+  await page.locator('[placeholder="Пароль"]').fill("123456");
+  await page.locator('[data-testid="login-submit-btn"]').click();
+  const error = await page.locator('[data-testid="login-error-hint"]');
+  await expect(error).toHaveText("Вы ввели неправильно логин или пароль");
+});
